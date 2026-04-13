8ff1bf93cae84f7d384fd1a5a61cf3c4cf81d63c
mariec-j <mcjardiniano@cpp.edu>
2026-04-13 15:14:32 -0700

    Update ALU_control.v
    - finished ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -3,6 +3,28 @@
 module ALU_Control(
     input [1:0] ALU_Op,
     input [5:0] SignExtend,
-    output [2:0] ALU_Control_outp
+    output reg [2:0] ALU_Control_outp
     );
+    
+    always @(*) begin
+        case (ALU_Op)
+            00: ALU_Control_outp = 010;
+            
+            01: ALU_Control_outp = 110;
+            
+            10: begin
+                case (SignExtend)
+                    100000: ALU_Control_outp = 010;
+                    100010: ALU_Control_outp = 110;
+                    100100: ALU_Control_outp = 000;
+                    100101: ALU_Control_outp = 001;
+                    101010: ALU_Control_outp = 111;
+                    default: ALU_Control_outp = 010;
+                endcase
+            end
+            
+            default: ALU_Control_outp = 010;
+        endcase
+    end
+    
 endmodule
