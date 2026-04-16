af60197b1fc22d611713da40f10bf4eb700576bb
mariec-j <mcjardiniano@cpp.edu>
2026-04-16 00:44:07 -0700

    Big Instruction_Execute.v fix
    - oopsies
    - its ok its fixed i think
    - shout out chatgpt for catching 2 of them involving ALU_Result wire naming missmatch

--- Instruction_Execute.v
+++ Instruction_Execute.v
@@ -8,7 +8,7 @@ module Instruction_Execute(
     input [31:0]    ReadData2,
     input           ALU_Src,
     input [31:0]    SignExtend,
-    input [3:0]     ALU_Op,
+    input [2:0]     ALU_Op,
     input [4:0]     Instr_2016,
     input [4:0]     Instr_1511,
     input           RegDst,
@@ -29,63 +29,73 @@ module Instruction_Execute(
 //internal wires the 5 outputs of aadder, ALU, MUX, ALUcontrol, Mux2
 wire [31:0] adder_32_out; //output of adder
 wire [31:0] mux_32_out; //mux 32 bit output
-wire [31:0] alu_32_out; // 32 bit output of alue
+wire [31:0] ALU_out; // 32 bit output of alue
 wire [2:0] ALU_control_out;//output of ALU control
 wire [4:0] mux_5_out;//output of second mux
-
+wire ALU_Zero_out;
 
 
 //Adder
-//
+adder adder01 (
     .inp_1(NPC),
     .inp_2(SignExtend),
     .Add_Result(adder_32_out)
+);
 
 //ALU_control
+ALU_control ALU_control01(
     .ALU_Op(ALU_Op),
     .SignExtend(SignExtend[5:0]),
-    .ALU_Control_outp(ALU_control_out),
+    .ALU_Control_outp(ALU_control_out)
+);
 
 //ALU
+ALU ALU01 (
     .ReadData1(ReadData1), //a
     .Data2(mux_32_out), //b
-    .control(ALU_control_out), //added bc she had this
-    .ALU_Result(alu_32_out),
-    .zero_flag(Zero)
+    .control(ALU_control_out), 
+    .ALU_Result(ALU_out),
+    .zero_flag(ALU_Zero_out)
+);
 
 //Mux_32bit
+mux #(.WIDTH_inp(32)) mux_32bit (
     .sel(ALU_Src),
-    .in_1(ReadData1),
+    .in_1(ReadData2),
     .in_2(SignExtend),
     .outp(mux_32_out)
+);
 
 //Mux_5bit
-    .sel(ALU_Src),
+mux #(.WIDTH_inp(5)) mux_5bit (
+    .sel(RegDst),
     .in_1(Instr_2016),
     .in_2(Instr_1511),
     .outp(mux_5_out)
+);
 
 //Latch
+EX_Mem_Latch EX_M_Latch (
 // - - - - - - inputs - - - - - -
-
     .WB(WB),
     .Mem(Mem),
-    .Add_Result(Add_Result), 
-    .Zero(Zero),
-    .ALU_Result(alu_32_out),
+    .Add_Result(adder_32_out), 
+    .Zero(ALU_Zero_out),
+    .ALU_Result(ALU_out),
     .ReadData2_ex_mem(ReadData2),
     .muxOut_5bit(mux_5_out),
     .clk(clk),
     .rst(rst),
 // - - - - - - outputs - - - - - -
-//THIS COMES FROM UP TOP OF FILE
+// Takes in all outputs and buffers for Memory stage
     .EX_Mem_Latch_WB(IE_WB),
     .EX_Mem_Latch_Mem(IE_Mem),
     .EX_Mem_Latch_Add_Result(Add_Result),
-   .EX_Mem_Latch_Zero(Zero),
+    .EX_Mem_Latch_Zero(Zero),
     .EX_Mem_Latch_ALU_Result(ALU_Result),
     .EX_Mem_Latch_ReadData2_ex_mem(ReadData2_ex_mem),
     .EX_Mem_Latch_muxOut_5bit(muxOut_5bit)
+);
 
 
 endmodule
