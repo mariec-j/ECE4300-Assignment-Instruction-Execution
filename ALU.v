88dbecb6558c7dccb22370a0f807457ab6632e7e
mariec-j <mcjardiniano@cpp.edu>
2026-04-15 18:05:16 -0700

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -8,28 +8,25 @@ module ALU(
     output zero_flag
     );
 
-
-parameter ALUadd = 3'b010,
-parameter ALUsub = 3'b110,
-parameter ALUand = 3'b000,     
-parameter ALUor = 3'b001,
+reg [31:0] result;
+parameter ALUadd = 3'b010;
+parameter ALUsub = 3'b110;
+parameter ALUand = 3'b000;     
+parameter ALUor = 3'b001;
 parameter ALUslt = 3'b111;
 
 always@* begin
     case(control)
-
-ALUadd: result = a + b;
-ALUsub: result = a - b;
-ALUand: result = a & b;
-ALUor: result = a | b;
-
-
-ALUslt: ALU_Result = ReadData1 < Data2 ? 1 - sign_mismatch // (1)
-: 0 + sign_mismatch; // (0)
-
-default: ALU_Result = 32'bX; // control = ALUx | *
+        ALUadd: result = ReadData1 + Data2;
+        ALUsub: result = ReadData1 - Data2;
+        ALUand: result = ReadData1 & Data2;
+        ALUor: result = ReadData1 | Data2;
+        ALUslt: result = ReadData1 < Data2 ? 1:0; // (0)
+        default: result = 32'bX; // control = ALUx 
     endcase
 end
+
 assign zero = (result == 0) ? 1 : 0;
+assign ALU_result = result;
 
 endmodule
