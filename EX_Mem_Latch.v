720c6607cf0f45ed3c63f80580d6eb9aeab7dc46
mariec-j <mcjardiniano@cpp.edu>
2026-04-15 19:05:10 -0700

    fixed EX_Mem_Latch.v
    forgor reg

--- EX_Mem_Latch.v
+++ EX_Mem_Latch.v
@@ -13,37 +13,37 @@ module EX_Mem_Latch(
     input           clk,
     input           rst,
 // - - - - - - outputs - - - - - -
-    output [1:0]    EX_Mem_Latch_WB,
-    output [2:0]    EX_Mem_Latch_Mem,
-    output [31:0]   EX_Mem_Latch_Add_Result,
-    output          EX_Mem_Latch_Zero,
-    output [31:0]   EX_Mem_Latch_ALU_Result,
-    output [31:0]   EX_Mem_Latch_ReadData2_ex_mem,
-    output [4:0]    EX_Mem_Latch_muxOut_5bit
+    output reg [1:0]    EX_Mem_Latch_WB,
+    output reg [2:0]    EX_Mem_Latch_Mem,
+    output reg [31:0]   EX_Mem_Latch_Add_Result,
+    output reg          EX_Mem_Latch_Zero,
+    output reg [31:0]   EX_Mem_Latch_ALU_Result,
+    output reg [31:0]   EX_Mem_Latch_ReadData2_ex_mem,
+    output reg [4:0]    EX_Mem_Latch_muxOut_5bit
     );
 
     always @(posedge clk or posedge rst)begin
     if(rst)begin
-//outputs
-EX_Mem_Latch_WB <= 2'b0;
-EX_Mem_Latch_Mem <= 3'b0;
-EX_Mem_Latch_Add_Result <= 32'b0;
-EX_Mem_Latch_Zero <= 0;
-EX_Mem_Latch_ALU_Result <= 32'b0;
-EX_Mem_Latch_ReadData2_ex_mem <= 32'b0;
-EX_Mem_Latch_muxOut_5bit <= 5'b0;
+        //outputs
+        EX_Mem_Latch_WB <= 2'b0;
+        EX_Mem_Latch_Mem <= 3'b0;
+        EX_Mem_Latch_Add_Result <= 32'b0;
+        EX_Mem_Latch_Zero <= 0;
+        EX_Mem_Latch_ALU_Result <= 32'b0;
+        EX_Mem_Latch_ReadData2_ex_mem <= 32'b0;
+        EX_Mem_Latch_muxOut_5bit <= 5'b0;
 
     end
 
    if(clk)begin
 //outputs
-EX_Mem_Latch_WB <= WB;
-EX_Mem_Latch_Mem <= Mem;
-EX_Mem_Latch_Add_Result <= Add_Result; 
-EX_Mem_Latch_Zero <= Zero;
-EX_Mem_Latch_ALU_Result <= ALU_Result;
-EX_Mem_Latch_ReadData2_ex_mem <= ReadData2_ex_mem;
-EX_Mem_Latch_muxOut_5bit <= muxOut_5bit;
+        EX_Mem_Latch_WB <= WB;
+        EX_Mem_Latch_Mem <= Mem;
+        EX_Mem_Latch_Add_Result <= Add_Result; 
+        EX_Mem_Latch_Zero <= Zero;
+        EX_Mem_Latch_ALU_Result <= ALU_Result;
+        EX_Mem_Latch_ReadData2_ex_mem <= ReadData2_ex_mem;
+        EX_Mem_Latch_muxOut_5bit <= muxOut_5bit;
 
    end
     end 
