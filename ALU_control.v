3626f45d677ac5a4afb98715b9f3027f9764ba5b
mariec-j <mcjardiniano@cpp.edu>
2026-04-16 00:54:43 -0700

    Update ALU_control.v
    - oopsies, i forgot how to write literals in verilog
    - fixed it

--- ALU_control.v
+++ ALU_control.v
@@ -8,22 +8,22 @@ module ALU_Control(
     
     always @(*) begin
         case (ALU_Op)
-            00: ALU_Control_outp = 010; // add
+            2'b00: ALU_Control_outp = 3'b010; // add
             
-            01: ALU_Control_outp = 110; // sub
+            2'b01: ALU_Control_outp = 3'b110; // sub
             
-            10: begin // R-type
+            2'b10: begin // R-type
                 case (SignExtend)
-                    100000: ALU_Control_outp = 010; // add
-                    100010: ALU_Control_outp = 110; // sub
-                    100100: ALU_Control_outp = 000; // AND
-                    100101: ALU_Control_outp = 001; // OR
-                    101010: ALU_Control_outp = 111; // set on less than
-                    default: ALU_Control_outp = 010; // default to add
+                    6'b100000: ALU_Control_outp = 3'b010; // add
+                    6'b100010: ALU_Control_outp = 3'b110; // sub
+                    6'b100100: ALU_Control_outp = 3'b000; // AND
+                    6'b100101: ALU_Control_outp = 3'b001; // OR
+                    6'b101010: ALU_Control_outp = 3'b111; // set on less than
+                    default: ALU_Control_outp = 3'b010; // default to add
                 endcase
             end
             
-            default: ALU_Control_outp = 010; // default to add
+            default: ALU_Control_outp = 3'b010; // default to add
         endcase
     end
     
