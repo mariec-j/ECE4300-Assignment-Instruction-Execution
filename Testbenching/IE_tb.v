063755e1978d3f26ff8068bc5d06bdf61e2e5a13
mariec-j <mcjardiniano@cpp.edu>
2026-04-15 18:35:51 -0700

    Working on IE_tb
    - added the various variables for the testbench
    - also moved file location

---
+++ Testbenching/IE_tb.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+
+module IE_tb();
+
+    reg [31:0]    NPC;
+// All the valuesredest bench  
+    reg [31:0]    ReadData1;
+    reg [31:0]    ReadData2;
+    reg           ALU_Src;
+    reg [31:0]    SignExtend;
+    reg [3:0]     ALU_Op;
+    reg [4:0]     Instr_2016;
+    reg [4:0]     Instr_1511;
+    reg           RegDst;
+    reg [1:0]     WB;
+    reg [2:0]     Mem;
+    reg           clk;
+    reg           rst;
+// - - - - - - outputs - - - - - -
+    wire [1:0]    IE_WB;
+    wire [2:0]    IE_Mem;
+    wire [31:0]   Add_Result;
+    wire          Zero;
+    wire [31:0]   ALU_Result;
+    wire [31:0]   ReadData2_ex_mem;
+    wire [4:0]    muxOut_5bit;
+
+Instruction_Execute UUT(
+// - - - - - - inputs - - - - - -
+    .NPC(),
+    .ReadData1(),
+    .ReadData2(),
+    .ALU_Src(),
+    .SignExtend(),
+    .ALU_Op(),
+    .Instr_2016(),
+    .Instr_1511(),
+    .RegDst(),
+    .WB(),
+    .Mem(),
+    .clk(),
+    .rst(),
+// - - - - - - outputs - - - - - -
+    .IE_WB(),
+    .IE_Mem(),
+    .Add_Result(),
+    .Zero(),
+    .ALU_Result(),
+    .ReadData2_ex_mem(),
+    .muxOut_5bit()
+);
+    
+    always forever #5 clk = ~clk ;    
+
+
+endmodule
