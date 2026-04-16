07a0d74822b72bc8ea01f7e6c951078b873e7fae
mariec-j <mcjardiniano@cpp.edu>
2026-04-16 02:52:17 -0700

    Update IE_tb.v
    - big write
    - add the diff parts of the test bench
    - im going to be praying

--- Testbenching/IE_tb.v
+++ Testbenching/IE_tb.v
@@ -3,12 +3,11 @@
 module IE_tb();
 
     reg [31:0]    NPC;
-// All the valuesredest bench  
     reg [31:0]    ReadData1;
     reg [31:0]    ReadData2;
     reg           ALU_Src;
     reg [31:0]    SignExtend;
-    reg [3:0]     ALU_Op;
+    reg [1:0]     ALU_Op;
     reg [4:0]     Instr_2016;
     reg [4:0]     Instr_1511;
     reg           RegDst;
@@ -50,11 +49,49 @@ Instruction_Execute UUT(
     .muxOut_5bit(muxOut_5bit)
 );
 
-    always forever #5 clk = ~clk;
+    always #5 clk = ~clk;
     
     initial begin
-    
-    end
+        NPC = 0;
+    	ReadData1 = 6;
+    	ReadData2 = 7;
+    	ALU_Src = 0;
+    	SignExtend = 0;
+    	ALU_Op = 0;
+    	Instr_2016 = 6;
+    	Instr_1511 = 7;
+    	RegDst = 0;
+    	WB = 3;        // At all times WB = 3
+    	Mem = 6;       // At all times Mem = 6
+    	clk = 0;
+    	rst = 0;
 
+    #5  rst = 1;
+// Test the adder 
+        NPC = 32'h10;
+        SignExtend = 32'h11;
+	// adder output should become 32'h21
+	
+// testing ALU_control and ALU
+// Changing ALU_Op and Sign extend to see if output is as intended
+    #10 ALU_Op = 0; // (Restating... lol) LW/SW,    ALU_control_out = 010,  ALU_Result = 13
+    #10 ALU_Op = 2'b01; // Branch Equal,               ALU_control_out = 110,  ALU_result = -1
+    #10 ALU_Op = 2'b10; // R-type,
+        SignExtend[5:0] = 32'b100000; // add,       ALU_control_out = 010,  ALU_Result = 13
+    #10 SignExtend[5:0] = 32'b100010; // sub,       ALU_control_out = 110,  ALU_Result = -1
+    #10 SignExtend[5:0] = 32'b100100; // AND,       ALU_control_out = 000,  ALU_Result = 6
+    #10 SignExtend[5:0] = 32'b100101; // OR,        ALU_control_out = 001,  ALU_Result = 7
+    #10 SignExtend[5:0] = 32'b101010; // slt,       ALU_control_out = 111,  ALU_Result = 1
+    #10 ReadData1 = 32'd10; // still slt,           ALU_control_out = 111,  ALU_Result = 0, Zero = 1
+    
+// test 32 bit mux
+    #10 ALU_Src = 0;    // Restating,       mux_32_out = ReadData2 = 7
+    #10 ALU_Src = 1; SignExtend = 32'h15; // mux_32_out = 32'h15
+    
+// test the 5 bit mux
+    #10 RegDst = 0;     // Restating,   muxOut_5bit = 5'h6
+    #10 RegDst = 1;     //              muxOut_5bit = 5'h7
+	
+    end
 
 endmodule
